61567a9f7faaa6832628733ffd018d56366216c9
Usuario <Usuario@CPD03-M07>
2024-10-21 14:55:29 -0300

    primeiro botao posto

--- MauiAppGuiaCurso/MainPage.xaml.cs
+++ MauiAppGuiaCurso/MainPage.xaml.cs
@@ -2,23 +2,26 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+       
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void PRIMEIRO_Clicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void SEGUNDO_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TERCEIRO_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 
